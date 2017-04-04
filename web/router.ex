a98952062fb94d1a142a49eeb2de252930bd2e58
sophatvathana <sovathana.phat@gmail.com>
2017-04-04 14:23:31 +0700

    Add swagger

--- web/router.ex
+++ web/router.ex
@@ -1,11 +1,13 @@
 defmodule TRexRestPhoenix.Router do
   use TRexRestPhoenix.Web, :router
+  use PhoenixSwagger
 
   pipeline :api do
     plug CORSPlug, [origin: "*"]
     plug BasicAuth, use_config: {:t_rex_rest_phoenix, :t_rex_security}
     plug :accepts, ["json"]
   end
+
 
   scope "/api/v1", TRexRestPhoenix do
     pipe_through :api
@@ -70,4 +72,17 @@ defmodule TRexRestPhoenix.Router do
   # scope "/api", TRexRestPhoenix do
   #   pipe_through :api
   # end
+  
+  scope "/api/swagger" do
+    forward "/", PhoenixSwagger.Plug.SwaggerUI, otp_app: :t_rex_rest_phoenix, swagger_file: "ttex.json"
+  end
+
+  def swagger_info do
+    %{
+      info: %{
+        version: "1.0",
+        title: "T-Tex-Web"
+      }    
+    }
+  end
 end
